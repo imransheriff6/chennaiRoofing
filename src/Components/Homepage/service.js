662f79b0c7cd86f3d0fd36dfde953567bd35781a
imransheriff6 <imransheriff6@gmail.com>
2024-03-15 10:50:57 +0530

    anime-build

--- src/Components/Homepage/service.js
+++ src/Components/Homepage/service.js
@@ -1,17 +1,21 @@
 import "./service.scss";
 function Service(p) {
-    return <div className="card">
-        <div className="part1">
-            <div className="icondiv"><img src={p.icon} alt="404" /></div>
-            <img src={p.image} alt="404" />
-            <div className="overlay"></div>
-            <div className="part2">
-                <div>
-                    <h2>{p.title}</h2>
-                    <p>{p.content}</p>
-                </div>
-            </div>
+  return (
+    <div className="card" data-aos={'zoom-out-up'} data-aos-delay={200}>
+      <div className="part1">
+        <div className="icondiv">
+          <img src={p.icon} alt="404" />
         </div>
+        <img src={p.image} alt="404" />
+        <div className="overlay"></div>
+        <div className="part2">
+          <div>
+            <h2>{p.title}</h2>
+            <p>{p.content}</p>
+          </div>
+        </div>
+      </div>
     </div>
+  );
 }
 export default Service;
